6f1a9c6515d4e14bc9622ce2a1a25ec978379822
diandrade <contato.andradediego@gmail.com>
2026-02-27 21:21:41 -0300

    feat: Introdução POO - Variáveis - 27/02.

---
+++ Introducao_OOP/Introducao_OOP/Introducao_dotNet/DN_01_Impressao.cs
@@ -0,0 +1,18 @@
+namespace Introducao_OOP.Introducao_dotNet;
+
+internal class Dn01Impressao
+{
+    public Dn01Impressao()
+    {
+        // Comentário em uma única linha 
+
+        /*
+         Comentário em múltiplas linhas
+        */
+
+        Console.WriteLine(new string('#', 50));
+        Console.WriteLine("Hello, World!");
+        Console.WriteLine(new string('#', 50));
+        Console.ReadKey();
+    }
+}
---
+++ Introducao_OOP/Introducao_OOP/Program.cs
@@ -0,0 +1,9 @@
+﻿string nome = "Nome da Pessoa";
+byte idade = 255;
+sbyte temperatura = 127;
+int valor = 23432432;
+float altura = 1.75f;
+bool estado = true;
+
+Console.WriteLine(nome);
+Console.WriteLine(idade);
